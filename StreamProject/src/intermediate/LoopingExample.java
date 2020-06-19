b1aadded6164eadb8598b186b9ff46c4356fd34f
admin <admin@YD03-01>
2020-06-19 13:55:05 +0900

    0619_Lambda_sort_peek

--- StreamProject/src/intermediate/LoopingExample.java
+++ StreamProject/src/intermediate/LoopingExample.java
@@ -16,7 +16,7 @@ public class LoopingExample {
 		System.out.println("[forEach()를 마지막에 호출한 경우]");
 		Arrays.stream(intArr).filter(a -> a%2 == 0).forEach(n -> System.out.println(n)); //foreach 중간처리메소드이나  최종 메소드로 동작
 		
-		//peek, forech 
+		//peek, foreach 
 		
 	}
 }
